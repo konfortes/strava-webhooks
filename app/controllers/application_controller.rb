f13d5a40da5f711defbfa37706a5e4196ce8449a
konfortes <ronen.konfortes@riskified.com>
2020-04-09 15:02:47 +0300

    removed unneeded files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+  def require_params(*keys)
+    keys.each_with_object(params) do |key, obj|
+      obj.require(key)
+    end
+  end
+
+  def ensure_authorization_token!
+    raise 'missing authorization token' unless current_user&.authorization_token
+  end
+
+  def strava_client
+    @strava_client ||= begin
+      user = User.where(uid: params[:owner_id]).first
+      access_token = user.fresh_authorization_token
+
+      Strava::Client.new(access_token: access_token)
+    end
+  end
 end
